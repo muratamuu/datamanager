942c911b835a59060f17262e4bc2edd45a27385f
Takashi, Murata <murata.muu@gmail.com>
2022-03-29 22:05:16 +0900

    SetRequest,GetRequest communication test

--- src/connection.rs
+++ src/connection.rs
@@ -7,19 +7,38 @@ use std::collections::HashMap;
 
 pub async fn serve(socket: TcpStream, store: &mut HashMap<Label, Value>) -> AppResult<()> {
 
+    let mut outbound = socket.clone();
     let mut from_client = utils::receive_as_json(BufReader::new(socket));
     while let Some(message_result) = from_client.next().await {
         let message = message_result?;
         match message {
             Message::GetDataRequest(r) => {
+                let mut results = Vec::new();
                 for label in r.params {
-                    println!("{:?}", store.get(&label));
+                    let value_ref = store.get(&label).unwrap_or(&Value::Int(-1));
+                    let value = match value_ref {
+                        Value::String(ref s) => Value::String(s.to_owned()),
+                        Value::Int(i) => Value::Int(*i),
+                        Value::Float(f) => Value::Float(*f),
+                    };
+                    results.push(LabeledValue{ label, value });
                 }
+                let response = Message::GetDataResponse(GetDataResponse {
+                    tag: r.tag,
+                    status: Status::OK,
+                    results,
+                });
+                utils::send_as_json(&mut outbound, &response).await?;
             }
             Message::SetDataRequest(r) => {
                 for LabeledValue { label, value } in r.params {
                     store.insert(label, value);
                 }
+                let response = Message::SetDataResponse(SetDataResponse {
+                    tag: r.tag,
+                    status: Status::OK,
+                });
+                utils::send_as_json(&mut outbound, &response).await?;
             }
             _ => (),
         }
@@ -30,16 +49,20 @@ pub async fn serve(socket: TcpStream, store: &mut HashMap<Label, Value>) -> AppR
 
 #[cfg(test)]
 mod test {
-    use crate::utils::AppResult;
+    use crate::utils::{self, AppResult};
+    use crate::message::*;
     use std::collections::HashMap;
+    use async_std::prelude::*;
+    use async_std::{net, task};
+    use async_std::io::BufReader;
 
     #[test]
     fn test_serve() {
-        use async_std::prelude::*;
-        use async_std::{net, task};
 
         task::block_on(async {
+
             let mut store = HashMap::new();
+
             // server
             let server_fut = async {
                 let listener = net::TcpListener::bind("localhost:8888").await?;
@@ -53,12 +76,59 @@ mod test {
 
             // client
             let client_fut = async {
+                // connect
                 let mut socket = net::TcpStream::connect("localhost:8888").await?;
-                let message = format!("{}\n{}\n",
-                    r#"{"command":"SetDataRequest", "params": [{"label":"SP1", "value":3.0}, {"label":"NE1", "value":10}]}"#,
-                    r#"{"command":"GetDataRequest", "tag":"ABC", "params": ["SP1"]}"#);
-                socket.write_all(message.as_bytes()).await?;
+                let mut from_client = utils::receive_as_json(BufReader::new(socket.clone()));
+
+                // send SetDataRequest
+                let message = Message::SetDataRequest(SetDataRequest {
+                    tag: Some("ABC".to_string()),
+                    params: vec![
+                        LabeledValue { label: "SP1".to_string(), value: Value::Float(3.0) },
+                        LabeledValue { label: "NE1".to_string(), value: Value::Int(10) },
+                    ],
+                });
+                utils::send_as_json(&mut socket, &message).await?;
+
                 task::yield_now().await;
+
+                // recv SendDataResponse
+                while let Some(message_result) = from_client.next().await {
+                    let message: Message = message_result?;
+                    if let Message::SetDataResponse(r) = message {
+                        assert_eq!(r.tag, Some("ABC".to_string()));
+                        assert_eq!(r.status, Status::OK);
+                    } else {
+                        assert!(false);
+                    }
+                    break;
+                }
+
+                // send GetDataRequest
+                let message = Message::GetDataRequest(GetDataRequest {
+                    tag: Some("123".to_string()),
+                    params: vec!["NE1".to_string(), "SP1".to_string()],
+                });
+                utils::send_as_json(&mut socket, &message).await?;
+
+                task::yield_now().await;
+
+                // recv GetDataResponse
+                while let Some(message_result) = from_client.next().await {
+                    let message: Message = message_result?;
+                    if let Message::GetDataResponse(r) = message {
+                        assert_eq!(r.tag, Some("123".to_string()));
+                        assert_eq!(r.status, Status::OK);
+                        assert_eq!(r.results[0].label, "NE1".to_string());
+                        assert_eq!(r.results[0].value, Value::Int(10));
+                        assert_eq!(r.results[1].label, "SP1".to_string());
+                        assert_eq!(r.results[1].value, Value::Float(3.0));
+                    } else {
+                        assert!(false);
+                    }
+                    break;
+                }
+
                 Ok(()) as AppResult<()>
             };
 
